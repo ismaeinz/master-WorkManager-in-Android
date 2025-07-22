d408498243ffb57114a3cb0ad91266d5aa80dc2f
ismaeinz <hire_me_ismaein@hotmail.com>
2025-07-22 07:11:35 +0300

    stop here

--- app/src/main/java/gaur/himanshu/workmanageryt/app/presentation/MainActivity.kt
+++ app/src/main/java/gaur/himanshu/workmanageryt/app/presentation/MainActivity.kt
@@ -4,22 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MainScreen()
         }
     }
 }
 
 
-@Composable
-fun MainScreen(modifier: Modifier = Modifier) {
-//https://youtu.be/us-GmyBeC3Q?t=656
-}
-
+//https://youtu.be/us-GmyBeC3Q?t=896
