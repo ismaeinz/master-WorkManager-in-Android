9a2e83f27304dd134d371c08e9c5fb391f26edb4
ismaeinz <hire_me_ismaein@hotmail.com>
2025-07-25 10:01:08 +0300

    little modification

--- app/src/main/java/gaur/himanshu/workmanageryt/app/presentation/MainActivity.kt
+++ app/src/main/java/gaur/himanshu/workmanageryt/app/presentation/MainActivity.kt
@@ -1,18 +1,110 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package gaur.himanshu.workmanageryt.app.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val viewModel = hiltViewModel<MainViewModel>()
+            MainScreen(viewModel) {
+                viewModel.getQuote()
+
+            }
         }
     }
 }
 
 
-//https://youtu.be/us-GmyBeC3Q?t=1131
+@Composable
+fun MainScreen(viewModel: MainViewModel, fetchQuote: () -> Unit) {
+    val uiState by viewModel.uiState.collectAsState()
+    Scaffold(topBar = {
+        TopAppBar(title = {
+            Text(text = "Quote s")
+        }, actions = {
+            IconButton(onClick = fetchQuote) {
+                Icon(imageVector = Icons.Default.Refresh, contentDescription = "")
+
+            }
+        })
+    }) {
+        if (uiState.data.isEmpty()) {
+            Box(
+                modifier = Modifier
+                    .padding(it)
+                    .fillMaxSize(), contentAlignment = Alignment.Center
+            ) {
+                Text(text = "Nothing Found")
+            }
+        } else {
+            LazyColumn(
+                modifier = Modifier
+                    .padding(it)
+                    .fillMaxSize()
+            ) {
+                items(uiState.data) { it ->
+                    @Composable {
+                        Card(
+                            modifier = Modifier
+                                .padding(8.dp)
+                                .fillMaxWidth()
+                        ) {
+                            Column(modifier = Modifier.padding(8.dp)) {
+                                Text(text = it.quote)
+                                Spacer(modifier = Modifier.height(8.dp))
+                                Text(text = "Author : ${it.author}")
+                                Spacer(modifier = Modifier.height(8.dp))
+                                Row(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.SpaceBetween
+                                ) {
+                                    Text(text = it.time.toString())
+                                    Text(text = it.workType)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+}
+//https://youtu.be/us-GmyBeC3Q?t=1587
